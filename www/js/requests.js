591294b831f625d8500dd764ee73f2f2183fdf9a
meda10 <32466683+meda10@users.noreply.github.com>
2021-12-01 21:19:44 +0100

    data post

--- www/js/requests.js
+++ www/js/requests.js
@@ -1,17 +1,35 @@
 const axios = require('axios');
 var autocomplete = require('autocompleter');
 
-var lidi = []
-var strany = []
-var kraje = []
+let lidi = []
+let strany = []
+let kraje = []
+let select_values = []
+
+function removeOptions(selectElement) {
+    var i, L = selectElement.options.length - 1;
+    for(i = L; i >= 0; i--) {
+        selectElement.remove(i);
+    }
+}
+function addOptions(selectElement) {
+    for (let i = 0; i < select_values.length; i++) {
+        let select = document.getElementById(selectElement);
+        let option = document.createElement("option");
+        option.text = select_values[i].label;
+        option.value = select_values[i].value;
+        select.add(option);
+    }
+}
+
 
 axios.get('http://localhost:5000/lidi', {}).then(function (response) {
     lidiRes = response.data;
     lidiRes.forEach(function(item){
-        let obj = { label : item.name, value: item.id }
+        let obj = { label : item.name, value: item.id, strana: item.strana }
         lidi.push(obj);
     });
-    get_lidi()
+    // get_lidi()
 }).catch(function (error) {
     console.log(error);
 });
@@ -47,7 +65,6 @@ function get_kraje(){
         fetch: function(text, update) {
             text = text.toLowerCase();
             var suggestions = kraje.filter(n => n.label.toLowerCase().startsWith(text))
-            console.log(suggestions)
             update(suggestions);
         },
         onSelect: function(item) {
@@ -64,11 +81,26 @@ function get_strany(){
         fetch: function(text, update) {
             text = text.toLowerCase();
             var suggestions = strany.filter(n => n.label.toLowerCase().startsWith(text))
-            console.log(suggestions)
             update(suggestions);
         },
         onSelect: function(item) {
             input.value = item.label;
+            select_values = []
+            lidi.forEach(function(it){
+                if(it.strana === item.label) {
+                    let obj = {label: it.label, value: it.value}
+                    select_values.push(obj);
+                }
+            });
+            removeOptions(document.getElementById('select_pref1'));
+            removeOptions(document.getElementById('select_pref2'));
+            removeOptions(document.getElementById('select_pref3'));
+            removeOptions(document.getElementById('select_pref4'));
+            addOptions('select_pref1')
+            addOptions('select_pref2')
+            addOptions('select_pref3')
+            addOptions('select_pref4')
+
         },
     });
 }
@@ -83,7 +115,6 @@ function get_lidi(){
         fetch: function(text, update) {
             text = text.toLowerCase();
             var suggestions = lidi.filter(n => n.label.toLowerCase().startsWith(text))
-            console.log(suggestions)
             update(suggestions);
         },
         onSelect: function(item) {
@@ -95,7 +126,6 @@ function get_lidi(){
         fetch: function(text, update) {
             text = text.toLowerCase();
             var suggestions = lidi.filter(n => n.label.toLowerCase().startsWith(text))
-            console.log(suggestions)
             update(suggestions);
         },
         onSelect: function(item) {
@@ -107,7 +137,6 @@ function get_lidi(){
         fetch: function(text, update) {
             text = text.toLowerCase();
             var suggestions = lidi.filter(n => n.label.toLowerCase().startsWith(text))
-            console.log(suggestions)
             update(suggestions);
         },
         onSelect: function(item) {
@@ -119,7 +148,6 @@ function get_lidi(){
         fetch: function(text, update) {
             text = text.toLowerCase();
             var suggestions = lidi.filter(n => n.label.toLowerCase().startsWith(text))
-            console.log(suggestions)
             update(suggestions);
         },
         onSelect: function(item) {
@@ -151,22 +179,23 @@ function handleSubmit(event) {
         }
     });
 
-    lidi.forEach(function(item){
-        if(value.pref1 === item.label){
-            pref1 = item.value
-        }
-        if(value.pref2 === item.label){
-            pref2 = item.value
-        }
-        if(value.pref3 === item.label){
-            pref3 = item.value
-        }
-        if(value.pref4 === item.label){
-            pref4 = item.value
-        }
-    });
-
-    const post_data = JSON.stringify({"records": [{"value": {"id_kraje": kraj_id, "strana": strana, "preferencni": [pref1, pref2, pref3, pref4]}}]});
+    // lidi.forEach(function(item){
+    //     if(value.pref1 === item.label){
+    //         pref1 = item.value
+    //     }
+    //     if(value.pref2 === item.label){
+    //         pref2 = item.value
+    //     }
+    //     if(value.pref3 === item.label){
+    //         pref3 = item.value
+    //     }
+    //     if(value.pref4 === item.label){
+    //         pref4 = item.value
+    //     }
+    // });
+    //
+    // const post_data = JSON.stringify({"records": [{"value": {"id_kraje": kraj_id, "strana": strana, "preferencni": [pref1, pref2, pref3, pref4]}}]});
+    const post_data = JSON.stringify({"records": [{"value": {"id_kraje": kraj_id, "strana": strana, "preferencni": [value.pref1, value.pref2, value.pref3, value.pref4]}}]});
     axios.post('http://localhost:8082/topics/hlasy', post_data, {
         headers: { 'Content-Type': 'application/vnd.kafka.json.v2+json' }
     }).then(function (response) {
